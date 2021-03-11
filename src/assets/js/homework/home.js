8c4ad92c671281c2990ad34125e9d16d5cdae491
Mateusz Niestrój <mateusz.niestroj@altimi.com>
2021-03-11 07:36:14 +0100

    👌 IMPROVE: Optimize init function

--- src/assets/js/homework/home.js
+++ src/assets/js/homework/home.js
@@ -1,160 +1,210 @@
-require(['./main'], function (main) {
+require(["./main"], function (main) {
+  require([
+    "jquery",
+    "components/domReady",
+    "tweenmax",
+    "components/bxslider",
+    "waypoints",
+    "global",
+  ], function ($, domReady, tweenmax, $bxslider, waypoints, global) {
+    var h = {
+      banner_anim: function () {
+        function start_anim() {
+          TweenLite.to(".home-banner__anim", 1, { opacity: 1, delay: 0.5 });
+          TweenLite.to("#platform, #pc", 1, {
+            top: "0",
+            ease: Back.easeOut,
+            delay: 0.5,
+          });
 
+          TweenLite.to("#card-base", 1.1, {
+            top: "-2%",
+            ease: Back.easeOut,
+            delay: 0.5,
+          });
+          TweenLite.to("#card-chip", 1.2, { top: "-3%", delay: 0.5 });
 
+          TweenLite.to("#email", 1.2, {
+            top: "-2%",
+            ease: Back.easeOut,
+            delay: 0.5,
+          });
 
+          TweenLite.to("#pc", 1.2, {
+            top: "-1%",
+            ease: Back.easeOut,
+            delay: 0.5,
+          });
 
-	require(['jquery', 'components/domReady', 'tweenmax', 'components/bxslider', 'waypoints', 'global'], function($, domReady, tweenmax, $bxslider, waypoints, global) {
+          TweenLite.to("#chart-base", 1, {
+            top: "0",
+            ease: Back.easeOut,
+            delay: 0.5,
+          });
+          TweenLite.to("#chart-1", 1, {
+            scaleY: 1,
+            top: "34.2%",
+            ease: Expo.easeOut,
+            delay: 0.5,
+          });
+          TweenLite.to("#chart-2", 1, {
+            scaleY: 1,
+            top: "41.7%",
+            ease: Expo.easeOut,
+            delay: 0.5,
+          });
+          TweenLite.to("#chart-3", 1, {
+            scaleY: 1,
+            top: "55.7%",
+            ease: Expo.easeOut,
+            delay: 0.5,
+          });
 
+          TweenLite.to("#cloud", 1.3, { top: 0, left: 0, ease: Expo.easeOut });
+          TweenLite.to("#social", 1.2, { top: 0, left: 0, ease: Expo.easeOut });
+          TweenLite.to("#mobile", 1.1, { top: 0, ease: Expo.easeOut });
+          TweenLite.to("#pc", 1.3, { top: 0, left: 0, ease: Expo.easeOut });
 
-		var h = {
+          TweenLite.to("#rainbow", 1.1, { opacity: 1, delay: 0.5, left: 0 });
+        }
+        /* init anim items and set initial properties */
+        TweenMax.set("#platform", { top: "-10%" });
+        TweenMax.set("#platform, #card-base, #card-chip, #email", {
+          top: "-10%",
+        });
 
-			banner_anim: function() {
+        TweenMax.set("#chart-base", { top: "5%" });
+        TweenMax.set("#chart-1", { scaleY: 0.1, top: "50.2%" });
+        TweenMax.set("#chart-2", { scaleY: 0.1, top: "53.8%" });
+        TweenMax.set("#chart-3", { scaleY: 0.1, top: "63.4%" });
 
+        TweenMax.set("#cloud", { left: "20%", top: "8%" });
+        TweenMax.set("#social", { left: "-10%", top: "-4%" });
+        TweenMax.set("#mobile", { top: "20%" });
+        TweenMax.set("#pc", { left: "10%", top: "-2%" });
 
-			    function start_anim() {
-					TweenLite.to('.home-banner__anim', 1, {opacity:1, delay:0.5});
-					TweenLite.to('#platform, #pc', 1, {top:'0', ease:Back.easeOut, delay:0.5});
+        TweenMax.set("#rainbow", { opacity: 0, left: "-5%" });
 
-					TweenLite.to('#card-base', 1.1, {top:'-2%', ease:Back.easeOut, delay:0.5});
-					TweenLite.to('#card-chip', 1.2, {top:'-3%', delay:0.5});
+        TweenLite.to(".home-banner__content p", 2, { opacity: 1, delay: 1.6 });
+        TweenLite.to(".l-home-banner__3-cols > div", 1, {
+          opacity: 1,
+          delay: 1.7,
+        });
+        TweenLite.to(".home-banner__content .headline-1", 2, {
+          opacity: 1,
+          delay: 1.1,
+        });
+        TweenLite.to(".home-banner__anim img", 1, { opacity: 1 });
+        TweenLite.to(".home-banner__content p .btn", 2, {
+          opacity: 1,
+          delay: 0,
+        });
+        TweenLite.to(".l-home-banner .btn--arrow-down", 2, {
+          opacity: 1,
+          delay: 2,
+        });
 
-					TweenLite.to('#email', 1.2, {top:'-2%', ease:Back.easeOut, delay:0.5});
+        start_anim();
+      },
 
-					TweenLite.to('#pc', 1.2, {top:'-1%', ease:Back.easeOut, delay:0.5});
+      // banner height adjustment
+      adjust_banner_height: function () {
+        var do_it = 0;
 
-					TweenLite.to('#chart-base', 1, {top:'0', ease:Back.easeOut, delay:0.5});
-					TweenLite.to('#chart-1', 1, {scaleY:1, top:'34.2%', ease:Expo.easeOut, delay:0.5});
-					TweenLite.to('#chart-2', 1, {scaleY:1, top:'41.7%', ease:Expo.easeOut, delay:0.5});
-					TweenLite.to('#chart-3', 1, {scaleY:1, top:'55.7%', ease:Expo.easeOut, delay:0.5});
+        function change_banner_size() {
+          if ($(window).width() > 1150 && $(window).height() > 700) {
+            var viewport_h = $(window).height(),
+              navbar_h = $(".l-navbar").height();
 
-					TweenLite.to('#cloud', 1.3, {top:0, left:0, ease:Expo.easeOut});
-					TweenLite.to('#social', 1.2, {top:0, left:0, ease:Expo.easeOut});
-					TweenLite.to('#mobile', 1.1, {top:0, ease:Expo.easeOut});
-					TweenLite.to('#pc', 1.3, {top:0, left:0, ease:Expo.easeOut});
+            var banner_height = viewport_h - navbar_h;
 
-					TweenLite.to('#rainbow', 1.1, {opacity:1, delay:0.5, left:0});
+            $(".l-home-banner").css({ height: banner_height });
+          } else {
+            $(".l-home-banner").css({ height: "auto" });
+          }
 
-			    }
+          if ($(window).width() > 1150 && $(window).height() > 1000) {
+            $(".l-home-banner").css({ height: "800px" });
+          }
+        }
 
+        change_banner_size();
+        this.banner_anim();
 
+        $(window).resize(function () {
+          clearTimeout(do_it);
+          do_it = setTimeout(function () {
+            change_banner_size();
+          }, 100);
+        });
+      },
 
+      slider_features: function () {
+        console.log("slider initial");
+        $("#slider-features").bxSlider({
+          nextSelector: ".btn-arrow--right",
+          prevSelector: ".btn-arrow--left",
+          nextText: "",
+          prevText: "",
+          pagerCustom: "#slider-features-pager",
+          touchEnabled: false,
+        });
+      },
 
+      info_box_anim: function () {
+        $(".info-box").waypoint(
+          function (direction) {
+            TweenMax.to(".info-box__product-tour img", 1.5, {
+              right: "-85%",
+              ease: Expo.easeOut,
+            });
+            TweenMax.to(".info-box__product-tour .btn", 2, {
+              opacity: 1,
+              delay: 0.5,
+              ease: Expo.easeOut,
+            });
+          },
+          { offset: 500 }
+        );
+      },
 
-					/* init anim items and set initial properties */
-				    TweenMax.set('#platform', {top:'-10%'});
-				    TweenMax.set('#platform, #card-base, #card-chip, #email', {top:'-10%'});
+      init: function () {
+        this.adjust_banner_height();
 
-				    TweenMax.set('#chart-base', {top:'5%'});
-				    TweenMax.set('#chart-1', {scaleY:0.1, top:'50.2%'});
-				    TweenMax.set('#chart-2', {scaleY:0.1, top:'53.8%'});
-				    TweenMax.set('#chart-3', {scaleY:0.1, top:'63.4%'});
+        var infobox = document.querySelector(".info-box");
+        var sliderFeatures = document.querySelector("#slider-features");
 
-				    TweenMax.set('#cloud', {left:'20%', top:'8%'});
-				    TweenMax.set('#social', {left:'-10%', top:'-4%'});
-				    TweenMax.set('#mobile', {top:'20%'});
-				    TweenMax.set('#pc', {left:'10%', top:'-2%'});
+        if (sliderFeatures) {
+          var sliderObserver = new IntersectionObserver((entries) => {
+            if (entries[0].intersectionRatio <= 0) return;
+            this.slider_features();
+            sliderObserver.disconnect();
+          });
+          // start observing
+          sliderObserver.observe(sliderFeatures);
+        }
 
-				    TweenMax.set('#rainbow', {opacity:0, left:'-5%'});
+        if (infobox) {
+          var infoBoxObserver = new IntersectionObserver((entries) => {
+            if (entries[0].intersectionRatio <= 0) return;
+            this.info_box_anim();
+            infoBoxObserver.disconnect();
+          });
+          // start observing
+          infoBoxObserver.observe(infobox);
+        }
 
+        /* slide to main section (button under main banner) */
+        $(".btn--arrow-down").on("click", function () {
+          var targetOffset = $("#main").offset().top + 5;
+          $("html,body").animate({ scrollTop: targetOffset }, 500);
 
-					TweenLite.to('.home-banner__content p', 2, {opacity:1, delay:1.6});
-					TweenLite.to('.l-home-banner__3-cols > div', 1, {opacity:1, delay:1.7});
-					TweenLite.to('.home-banner__content .headline-1', 2, {opacity:1, delay:1.1});
-					TweenLite.to('.home-banner__anim img', 1, {opacity:1});
-					TweenLite.to('.home-banner__content p .btn', 2, {opacity:1, delay:0});
-					TweenLite.to('.l-home-banner .btn--arrow-down', 2, {opacity:1, delay:2});
+          return false;
+        });
+      },
+    };
 
-
-				    start_anim();
-
-			},
-
-
-			// banner height adjustment
-			adjust_banner_height: function() {
-				var do_it = 0;
-
-
-				function change_banner_size() {
-					if($(window).width() > 1150 && $(window).height() > 700)
-					{
-						var viewport_h = $(window).height(),
-							navbar_h = $('.l-navbar').height()
-
-						var banner_height = viewport_h - navbar_h;
-
-						$('.l-home-banner').css({'height':banner_height});
-					}
-					else {
-						$('.l-home-banner').css({'height':'auto'});
-					}
-
-					if($(window).width() > 1150 && $(window).height() > 1000)
-					{
-						$('.l-home-banner').css({'height':'800px'});
-					}
-				}
-
-
-				change_banner_size();
-				this.banner_anim();
-
-
-				$(window).resize(function(){
-					clearTimeout(do_it);
-					do_it = setTimeout(function(){change_banner_size();}, 100);
-				});
-			},
-
-
-
-			slider_features: function()
-			{
-	            $('#slider-features').bxSlider({
-	                nextSelector: '.btn-arrow--right',
-	                prevSelector: '.btn-arrow--left',
-	                nextText: '',
-	                prevText: '',
-	                pagerCustom: '#slider-features-pager',
-	                touchEnabled: false
-	            });
-			},
-
-			info_box_anim: function() {
-
-				$('.info-box').waypoint(function(direction) {
-					TweenMax.to('.info-box__product-tour img', 1.5, {right:'-85%', ease:Expo.easeOut});
-					TweenMax.to('.info-box__product-tour .btn', 2, {opacity:1, delay: 0.5, ease:Expo.easeOut});
-				}, { offset: 500 });
-
-
-			},
-
-
-
-			init: function() {
-				this.adjust_banner_height();
-				this.slider_features();
-				this.info_box_anim();
-
-
-				/* slide to main section (button under main banner) */
-				$('.btn--arrow-down').on('click', function()
-				{
-					var targetOffset = $('#main').offset().top+5;
-					$('html,body').animate({scrollTop:targetOffset}, 500);
-
-					return false;
-				});
-
-
-			}
-		};
-
-		h.init();
-		global.init();
-	});
-
-
-
+    h.init();
+    global.init();
+  });
 });
